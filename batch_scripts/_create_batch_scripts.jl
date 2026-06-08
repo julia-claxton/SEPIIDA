813ee15660ea73763f4c7b62fd7f5451e5b989d3
Julia C <julia.claxton@colorado.edu>
2026-06-08 13:05:38 -0600

    Mon Jun  8 19:05:38 UTC 2026

--- batch_scripts/_create_batch_scripts.jl
+++ batch_scripts/_create_batch_scripts.jl
@@ -71,47 +71,43 @@ function status()
     println("\nThere are $(length(jobs)) jobs.")
 end
 
-
 # Remove existing jobscripts
 rm.(glob("*.sh", @__DIR__))
 
 # Write desired jobs
-for pa in 90:1:110
-    N = 1e3
-    E = 1e3
-    write_job_script("preemptable", N, "e-", E, pa, 
-        prefix = "finding_losscone",
-        flags = "
-            -magnetic_model jrm33
-            -atmosphere_filename jupiter_gram.csv
-            -injection_altitude 990.0
-            -backscatter_altitude 991.0
-            -brem_splitting 1
-            -min_energy_eV 10
-            -lat 85
-        "
-    )
+#=
+for E in logrange(1e1, 1e5, 30)
+    for pa in 100:5:180
+        N = 1e3
+        write_job_script("preemptable", N, "e-", E, pa, 
+            prefix = "finding_losscone",
+            flags = "
+                -magnetic_model jrm33
+                -atmosphere_filename jupiter_gram.csv
+                -injection_altitude 990.0
+                -backscatter_altitude 991.0
+                -brem_splitting 1
+                -min_energy_eV 10
+                -lat 85
+                -cache_radius_km 1.0
+            "
+        )
+    end
 end
+=#
 
-for r in logrange(0.01, 10, 10)
-    N = 1e3
-    E = 1e3
-    pa = 120
-    r_string = "$(round(r, digits = 2))"
-    write_job_script("preemptable", N, "e-", E, pa, 
-        prefix = "cacheradius_$(r_string)",
-        flags = "
-            -magnetic_model jrm33
-            -atmosphere_filename jupiter_gram.csv
-            -injection_altitude 990.0
-            -backscatter_altitude 991.0
-            -brem_splitting 1
-            -min_energy_eV 10
-            -cache_radius_km $(r_string)
-            -lat 85
-        "
-    )
-end
-
+write_job_script("preemptable", 1e5, "e-", 1e4, 120, 
+    prefix = "runtime_test",
+    flags = "
+        -magnetic_model jrm33
+        -atmosphere_filename jupiter_gram.csv
+        -injection_altitude 990.0
+        -backscatter_altitude 991.0
+        -brem_splitting 100
+        -min_energy_eV 10
+        -lat 85
+        -cache_radius_km 1.0
+    "
+)
 
 status()
